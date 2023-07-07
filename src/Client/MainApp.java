ce354e913f381d81df43da59504b510d0e2cc092
Sh1rsh1n <a.s.shirshin@mail.ru>
2023-07-07 21:03:05 +0300

    add server\client files

---
+++ src/Client/MainApp.java
@@ -0,0 +1,7 @@
+package Client;
+
+public class MainApp {
+    public static void main(String[] args) {
+        new ClientWindow();
+    }
+}
---
+++ src/Server/MainApp.java
@@ -0,0 +1,7 @@
+package Server;
+
+public class MainApp {
+    public static void main(String[] args) {
+        new Server();
+    }
+}
